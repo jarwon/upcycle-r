6be10d1a3e4b5acbb0822d548aa7607fb0e9d3b0
jade w <jadarz@gmail.com>
2022-06-07 14:36:58 -0400

    #3: init activity page

---
+++ src/pages/activity/[id].js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../../components/layout";
+
+const Activity = () => {
+  return (
+    <Layout>
+      <p>activity page</p>
+    </Layout>
+  );
+};
+
+export default Activity;
