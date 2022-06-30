64f47e2ba00820dcfdf0892fb44d9a59f5c019b0
jade w <jadarz@gmail.com>
2022-06-30 11:13:50 -0400

    #1: activities page refactor

--- src/pages/activities.js
+++ src/pages/activities.js
@@ -1,87 +1,68 @@
 // Step 1: Import React
 import * as React from "react";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import Layout from "../components/layout";
 import ActivityTile from "../components/activity-tile";
 import { navigate } from "gatsby";
+import { getActivities, getActivity } from "../../utilities/strava";
 
-const clientId = 83167;
-const clientSecret = "36073471fd5bf8f963ade25fe5642a5699c0dc4d";
-const token = "c9f0108dd563920328846970fed21a1a7311c0e5";
-const baseUrl = "https://www.strava.com/api/v3";
+const sortActivitiesByDate = (activities) => {
+  return activities.sort((a, b) => {
+    return Date.parse(b.start_date) - Date.parse(a.start_date);
+  });
+};
 
-// Step 2: Define your component
 const ActivitiesPage = () => {
   //define state
-  // const [ActivityIds, setActivityIds] = useState();
   const [activities, setActivities] = useState();
   const [typeOfActivity, setTypeOfActivity] = useState("all");
+  const renderCounter = useRef(0);
+  renderCounter.current = renderCounter.current + 1;
 
-  const getData = () => {
-    const url = `${baseUrl}/athlete/activities?scope=activity:read_all&per_page=3"`;
-
-    fetch(url, {
-      method: "GET",
-      headers: { Authorization: `Bearer ${token}` },
-    })
-      .then((res) => res.json())
-      .then((activitiesData) => {
+  const getData = async () => {
+    try {
+      const res = await getActivities({
+        scope: "activity:read_all",
+        per_page: 3,
+      });
+      if (res.ok) {
+        const activitiesData = await res.json();
         console.log(activitiesData);
-        const ids = activitiesData.map((activity) => activity.id);
-        ids.forEach((id) => {
-          getActivity(id);
-        });
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
-
-  const getActivity = (id) => {
-    const url = `${baseUrl}/activities/${id}`;
-    fetch(url, {
-      method: "GET",
-      headers: { Authorization: `Bearer ${token}` },
-    })
-      .then((res) => res.json())
-      .then((activity) => {
-        // if Activities was an object: Activities['kasdjkasdja']
-
-        // if Activities was an array Activities.find(activity => activity.id === id)
-
-        setActivities((prevState) => {
-          if (!prevState) {
-            return [activity];
-          }
-          return [...prevState, activity];
-        });
-      });
+        const activitiesTemp = await Promise.all(
+          activitiesData.map(async ({ id }) => {
+            // return await getActivity(id);
+            try {
+              const resId = await getActivity(id);
+              if (resId.ok) {
+                return await resId.json();
+              }
+            } catch (error) {
+              console.error(error);
+            }
+          })
+        );
+        setActivities(activitiesTemp);
+      }
+      // throw new Error(res.status);
+    } catch (error) {
+      console.error(error);
+      // navigate("/");
+    }
   };
 
   useEffect(() => {
     getData();
   }, []);
 
-  // useEffect(() => {
-  //   if (!Activities) return;
-  //   const activitiesWithCalories = Activities.forEach((activity) => {
-  //     getCalories(activity.id);
-  //   });
-  // }, [Activities]);
-
-  // const filteredActivities = activities.filter((activity) =>
-  //   activity.type.toLowerCase().includes(typeOfActivity.toLowerCase())
-  // );
-
   //early return
   if (!activities) {
-    return <div>Loading</div>;
+    return <div>loading</div>;
   }
 
   return (
     <Layout>
       <div>
-        <label htmlFor="activity-select">Filter Activities</label>
+        <label htmlFor="activity-select">filter activities by type</label>
 
         <select
           name="activities"
@@ -89,45 +70,35 @@ const ActivitiesPage = () => {
           onChange={(e) => setTypeOfActivity(e.target.value)}
           value={typeOfActivity}
         >
-          <option value="all">Please choose an option</option>
-          <option value="ride">Ride</option>
-          <option value="virtual ride">Virtual Ride</option>
-          <option value="run">Run</option>
-          <option value="walk">Walk</option>
+          <option value="all">all</option>
+          <option value="ride">ride</option>
+          <option value="virtualride">virtual ride</option>
+          <option value="run">run</option>
+          <option value="walk">walk</option>
         </select>
       </div>
       <div>
-        <p>Activities:</p>
+        <p>activities:</p>
+        <h1>Renders: {renderCounter.current}</h1>
 
         <div className="activity">
-          {activities.map((activity) => {
+          {sortActivitiesByDate(activities)?.map((activity) => {
             if (
               activity.type.toLowerCase() === typeOfActivity.toLowerCase() ||
               typeOfActivity === "all"
             )
               return (
-                <div key={activity.id}>
-                  <ActivityTile
-                    activities={activities}
-                    name={activity.name}
-                    averageSpeed={activity.average_speed}
-                    averageWatts={activity.average_watts}
-                    calories={activity.calories}
-                    sufferScore={activity.sufferScore}
-                    type={activity.type}
-                  />
-                  <button
-                    onClick={(event) => {
-                      event.preventDefault();
-                      console.log(activity);
-                      navigate(`/activity/${activity.id}`, {
-                        state: { activities },
-                      });
-                    }}
-                  >
-                    go to activity
-                  </button>
-                </div>
+                <ActivityTile
+                  handleClick={(event) => {
+                    event.preventDefault();
+                    console.log(activity);
+                    navigate(`/activity/${activity.id}`, {
+                      state: { activity },
+                    });
+                  }}
+                  activity={activity}
+                  key={activity.id}
+                />
               );
           })}
         </div>
@@ -136,5 +107,4 @@ const ActivitiesPage = () => {
   );
 };
 
-// Step 3: Export your component
 export default ActivitiesPage;
