06f9cbf2611c07fe5ea60f88dcbf6c11b45d025b
jade w <jadarz@gmail.com>
2022-06-30 11:13:07 -0400

    #5: profile component

--- src/components/profile.js
+++ src/components/profile.js
@@ -1,46 +1,55 @@
 import React, { useState, useEffect } from "react";
+import { getAthlete } from "../../utilities/strava";
 
 const Profile = () => {
-  //define state
-  const [User, setUser] = useState();
+  const [user, setUser] = useState();
+  const [ftpToggle, setFtpToggle] = useState(false);
+  const [error, setError] = useState(null);
 
-  const getData = () => {
-    const url = "https://www.strava.com/api/v3/athlete";
-    const clientId = 83167;
-    const clientSecret = "36073471fd5bf8f963ade25fe5642a5699c0dc4d";
-    const token = "c6ec1af1bd27a74fced84a48bd0e1558504bfb52";
-
-    fetch(url, {
-      method: "GET",
-      headers: { Authorization: `Bearer ${token}` },
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-        setUser(data);
-      });
+  const getData = async () => {
+    try {
+      const res = await getAthlete();
+      if (res.ok) {
+        const userData = await res.json();
+        setUser(userData);
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   useEffect(() => {
     getData();
   }, []);
 
-  //early return
-  if (!User) {
-    return <div>Loading</div>;
-  }
+  const calcWKg = () => {
+    return `${(user.ftp / user.weight).toFixed(2)} w/kg`;
+  };
 
   return (
     <div>
-      <header className="flex">
-        <div>
-          <p>{User.firstname}</p>
-          <p>{User.lastname}</p>
-        </div>
-        <div>
-          <p>{User.city}</p>
-          <p>{User.country}</p>
-        </div>
+      <header className="flex lowercase">
+        {user && (
+          <>
+            <div className="mr-3">
+              <img src={user.profile} />
+            </div>
+            <div>
+              <p>
+                {user.firstname} {user.lastname}
+              </p>
+              <p>
+                {user.city}, {user.country}
+              </p>
+              <p>
+                {user.weight}kg, {(user.weight * 2.205).toFixed(0)}lbs
+              </p>
+              <p className="font-bold">
+                {user.ftp}W, {calcWKg()}
+              </p>
+            </div>
+          </>
+        )}
       </header>
     </div>
   );
