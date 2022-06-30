52d5921b8663c5f0c76d45b94a07f629d5e52544
jade w <jadarz@gmail.com>
2022-06-30 11:22:33 -0400

    #3: test activity page

--- src/pages/activity/[id].js
+++ src/pages/activity/[id].js
@@ -1,12 +1,22 @@
 import React from "react";
 import Layout from "../../components/layout";
+import { useState, useEffect } from "react";
 
-const Activity = () => {
+const ActivityPage = ({ location }) => {
+  const {
+    state: { activity },
+  } = location;
+  console.log(activity);
+  const { name } = activity;
+
+  useEffect(() => {}, []);
+
   return (
     <Layout>
       <p>activity page</p>
+      {name}
     </Layout>
   );
 };
 
-export default Activity;
+export default ActivityPage;
