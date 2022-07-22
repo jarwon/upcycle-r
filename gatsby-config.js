f9d7385ccdeb95e86ce3f80b5b315bab3761d4bd
jade w <jadarz@gmail.com>
2022-07-22 16:30:42 -0400

    gatsby config, package config

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ require("dotenv").config({
 module.exports = {
   siteMetadata: {
     title: `upcycle-r`,
-    siteUrl: `https://www.yourdomain.tld`,
+    siteUrl: `http://localhost:8000`,
   },
   plugins: [
     {
@@ -13,6 +13,22 @@ module.exports = {
       options: {
         // Configure SASS to process Tailwind
         postCssPlugins: [require("tailwindcss")],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-breadcrumb`,
+      options: {
+        // defaultCrumb: optional To create a default crumb
+        // see Click Tracking default crumb example below
+        // defaultCrumb: {
+        //   location: {
+        //     pathname: "/",
+        //   },
+        //   crumbLabel: "HomeCustom",
+        //   crumbSeparator: " / ",
+        // },
+        // usePathPrefix: optional, if you are using pathPrefix above
+        // usePathPrefix: "/blog",
       },
     },
     "gatsby-plugin-postcss",
